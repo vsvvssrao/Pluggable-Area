79f8c0bacecf43f7727fee5a9a0632d7ccb7dbbc
vsvvssrao <vsvvssrao@gmail.com>
2019-11-21 21:56:46 +0530

    Add project files.

---
+++ WebApplication2/Areas/ChildArea/ChildArea/App_Start/ChildAreaAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChildArea.App_Start
+{
+    public class ChildAreaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "ChildArea";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ChildArea_default",
+                "ChildArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "ChildArea.Controllers" }
+            );
+        }
+    }
+}
